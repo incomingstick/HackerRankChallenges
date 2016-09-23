4f652a78baeb680f17b763d043f152e33250c4ab
incomingstick <incomingstick@gmail.com>
2016-09-23 13:04:23 -0500

    Added Algorithms/Search/Pairs

---
+++ Algorithms/Search/Pairs/README.md
@@ -0,0 +1,29 @@
+# Pairs
+Given **_N_** integers, count the number of pairs of integers whose difference is **_K_**.
+
+## Input Format
+
+The first line contains **_N_** and **_K_**.  
+The second line contains **_N_** numbers of the set. All the **_N_** numbers are unique.
+
+## Output Format
+
+An integer that tells the number of pairs of integers whose difference is **_K_**.
+
+## Constraints:
+**_N_ <= 10^5**  
+**0 < _K_ < 10^9**  
+Each integer will be greater than **0** and at least **_K_** smaller than **2^31 - 1**.
+
+## Sample Input
+```
+5 2  
+1 5 3 4 2  
+```
+## Sample Output
+```
+3
+```
+## Explanation
+
+There are 3 pairs of integers in the set with a difference of 2.
---
+++ Algorithms/Search/Pairs/answer.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int pairs(int a_size, int* a, int k) {
+    int ans = 0;
+    
+    int i = 0, j = 1;
+    while(j < a_size) {
+        int diff = a[j] - a[i];
+
+        if(diff > k) {
+            i++;
+        } else if(diff < k) {
+            j++;
+        } else {
+            ans++;
+            j++;
+        }
+    }
+
+    return ans;
+}
+
+/* Comparison function to help sort the 
+   calling the pairs function. */
+int comp (const void * elem1, const void * elem2) 
+{
+    int f = *((int*)elem1);
+    int s = *((int*)elem2);
+    if (f > s) return  1;
+    if (f < s) return -1;
+    return 0;
+}
+
+int main() {
+    int res;
+    int _a_size, _a_i, _k;
+
+    scanf("%d %d", &_a_size, &_k);
+
+    int _a[_a_size];
+
+    for(_a_i = 0; _a_i < _a_size; _a_i++) { 
+        int _a_item;
+        scanf("%d", &_a_item);
+        
+        _a[_a_i] = _a_item;
+    }
+    
+    qsort(_a, sizeof(_a)/sizeof(*_a), sizeof(*_a), comp);
+
+    res = pairs(_a_size, _a, _k);
+    
+    printf("%d\n",res);   
+    
+    return 0;
+}
