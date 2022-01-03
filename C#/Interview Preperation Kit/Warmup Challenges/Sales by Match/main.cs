6b8186634e2325904c6ffa61829f55707580ad52
Nick Gaulke <incom@Desktop.gaulke.xyz>
2022-01-03 14:16:09 -0600

    Submit C# Sales by Match

---
+++ C#/Interview Preperation Kit/Warmup Challenges/Sales by Match/main.cs
@@ -0,0 +1,80 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<int> ar)
+    {
+        // Lets get it sorted to make popping and collecting easier
+        ar.Sort();
+
+        // Print it so we can visualize
+        //Console.WriteLine("[{0}]", string.Join(", ", ar));
+
+        int curr = ar[0];   // The current number being evaluated
+        int count = 0;      // The number of times we have seen the curr
+        int pairs = 0;      // The number of total pairs across the set
+        int it = 0;         // The iteration total
+
+        foreach(int val in ar) {
+            // We can skip the first value, as that is the first curr
+            if(it++ == 0) {
+                continue;
+            }
+            // Check if the val is our curr, if it is, check if we are on a set pair
+            else if(curr == val) {
+                if(count++ % 2 == 0) {
+                    pairs++;
+                }
+            }
+            // We have found a new curr val, reset the curr count
+            else {
+                curr = val;
+                count = 0;
+            }
+        }
+
+        return pairs;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.sockMerchant(n, ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
