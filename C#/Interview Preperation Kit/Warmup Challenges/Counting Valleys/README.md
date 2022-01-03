a9a9111d9d27ba2973bda636cff47c8d1ef20a2e
incomingstick <incomingstick@gmail.com>
2022-01-03 14:55:16 -0600

    add Counting Valleys challenge and update gitignore

---
+++ C#/Interview Preperation Kit/Warmup Challenges/Counting Valleys/README.md
@@ -0,0 +1,53 @@
+# Sales by Match
+An avid hiker keeps meticulous records of their hikes. During the last hike that took exactly **_steps_** steps, for every step it was noted if it was an uphill, **_U_**, or a downhill, **_D_** step. Hikes always start and end at sea level, and each step up or down represents a **_1_** unit change in altitude. We define the following terms:
+
+- A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
+- A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
+
+Given the sequence of up and down steps during a hike, find and print the number of valleys walked through.
+
+## Example
+**_steps_ = _8 path_ = [DDUUUUDD]**
+
+The hiker first enters a valley **_2_** units deep. Then they climb out and up onto a mountain **_2_** units high. Finally, the hiker returns to sea level and ends the hike.
+
+## Function Description
+Complete the countingValleys function in the editor below.
+
+countingValleys has the following parameter(s):
+
+- int steps: the number of steps on the hike
+- string path: a string describing the path
+
+## Returns
+- int: the number of valleys traversed
+
+## Input Format
+The first line contains an integer **_2_**, the number of steps in the hike.
+
+The second line contains a single string **_steps_**, of **_steps_** characters that describe the path.
+
+## Constraints 
+**2 <= _steps_ <= 10^6**  
+**_path_[_i_] some set of {UD}**  
+
+## Sample Input
+```
+8
+UDDDUDUU
+```
+## Sample Output
+```
+1
+```
+
+## Explanation
+If we represent _ as sea level, a step up as /, and a step down as \, the hike can be drawn as:
+
+```
+_/\      _
+   \    /
+    \/\/
+```
+
+The hiker enters and leaves one valley.
---
+++ C#/Interview Preperation Kit/Warmup Challenges/Counting Valleys/main.cs
@@ -0,0 +1,79 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+    public static int countingValleys(int steps, string path)
+    {
+        // Print it so we can visualize
+        //Console.WriteLine("[{0}]", path);
+
+        int valleys = 0;            // The number of valleys we have traversed
+        bool isCurrValley = false;  // Tracks if we are currently in a valley to track when we enter and leave
+        int level = 0;              // Tracks the current level above "sea level"
+
+        foreach(char c in path) {
+            // When 'U' or 'u', we go up a level
+            if(c == 'U' || c == 'u') {
+                level++;
+            }
+            // When 'D' or 'd', we go down a level
+            else if (c == 'D' || c == 'd') {
+                level--;
+            }
+
+            // If we are at or above sea level, we are no longer in a valley
+            if(level >= 0) {
+                isCurrValley = false;
+            }
+            // When level is below 0, we are in a valley, and if we were not already in a valley, we have entered a new one!
+            else if(level < 0 && !isCurrValley) {
+                valleys++;
+                isCurrValley = true;
+            }
+        }
+
+        return valleys;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int steps = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string path = Console.ReadLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
