0006f67c01d621462630dd91ebf009027dbbc4f1
incomingstick <incomingstick@gmail.com>
2022-03-21 21:08:00 -0500

    remove stale using statements from C# challenges

--- C#/Interview Preperation Kit/Arrays/Arrays - Left Rotation/main.cs
+++ C#/Interview Preperation Kit/Arrays/Arrays - Left Rotation/main.cs
@@ -1,15 +1,6 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
 using System;
 
 class Result
